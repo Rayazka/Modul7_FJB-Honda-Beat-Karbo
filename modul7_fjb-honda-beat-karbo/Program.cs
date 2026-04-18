02ace59a4fa39e454a1dd171e3539a53c86a3322
nasywa <nasywazahranailah@gmail.com>
2026-04-18 08:32:58 +0700

    Menambahkan class FilmFavorit

--- modul7_fjb-honda-beat-karbo/Program.cs
+++ modul7_fjb-honda-beat-karbo/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul7_fjb_honda_beat_karbo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FilmFavorit_103022400105 filmFavFarrel = new FilmFavorit_103022400105();
+            filmFavFarrel.ReadJSON();
+        }
+    }
+}
