aa2cea24e44004e13af376d8830a2ece3c577bcc
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-18 09:16:06 +0700

    Message

--- modul7_fjb-honda-beat-karbo/Program.cs
+++ modul7_fjb-honda-beat-karbo/Program.cs
@@ -11,6 +11,9 @@
             Watchlist_103022400093 dataWatchlistToni = new Watchlist_103022400093();
             dataWatchlistToni.ReadJSON();
             Console.WriteLine();
+
+            GenreDictionary_103022400093 dataGenreToni = new GenreDictionary_103022400093();
+            dataGenreToni.ReadJSON();
         }
     }
 }
