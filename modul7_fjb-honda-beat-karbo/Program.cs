6e00e9ef801c87fe1294dbdcb4734082fc5d091b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-18 09:05:07 +0700

    Message

--- modul7_fjb-honda-beat-karbo/Program.cs
+++ modul7_fjb-honda-beat-karbo/Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
-            FilmFavorit_103024400093 dataFilmToni= new FilmFavorit_103024400093();
+            FilmFavorit_103024400093 dataFilmToni = new FilmFavorit_103024400093();
             dataFilmToni.ReadJson();
+            Console.WriteLine();
+
+            Watchlist_103022400093 dataWatchlistToni = new Watchlist_103022400093();
+            dataWatchlistToni.ReadJSON();
+            Console.WriteLine();
         }
     }
 }
